287c6597019e0b47cbad25b8610e688a1b39ea8b
Fennikz <tripop.xtarnos@mail.kmutt.ac.th>
2023-03-08 16:45:03 +0700

    .

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Fractal : MonoBehaviour
 {
+    List<GameObject> cubes = new List<GameObject>();
+
     [SerializeField]
     private GameObject cubePrefab;
 
@@ -16,15 +18,62 @@ public class Fractal : MonoBehaviour
     void Start()
     {
         //  This statement create a game object given a prefab, position and rotation.
-        Instantiate(
+        GameObject go = Instantiate(
             //  This parameter sets a prefab for an instance to be created.
-            cubePrefab,
-
+            cubePrefab, 
             //  This parameter sets an instance position to (0, 0, 0).
             Vector3.zero,
-
             //  This parameter sets an instance rotation to (0, 0, 0).
-            Quaternion.identity
-        );
+            Quaternion.identity );
+         go.transform.localScale = new Vector3(size, size, size);
+         go.GetComponent<MengerBox>().size = size;
+        cubes.Add(go);
+    }
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            List<GameObject> newCubes = Split(cubes);
+            foreach (var go in cubes)
+            {
+                Destroy(go);
+            }
+            cubes = newCubes;
+        }
+    }
+        List<GameObject> Split(List<GameObject> cubes)
+        {
+           List<GameObject> subCubes = new List<GameObject>();
+           foreach (var cube in cubes)
+     {
+            float size = cube.GetComponent<MengerBox>().size;
+        for (int x = -1 ; x < 2; x++)
+        {
+            for(int y = -1; y < 2; y++)
+            {
+                for(int z = -1; z < 2; z++)
+                {
+                    float xx = x * ((size / 3f)*3f);
+                    float yy = y * ((size / 3f)*3f);
+                    float zz = z * ((size / 3f)*3f);
+
+                    Vector3 cubePos = new Vector3(xx, yy, zz)
+                    +cube.transform.position;
+
+                    int sum = Mathf.Abs(x) + Mathf.Abs(y) + Mathf.Abs(z) ;
+                    if (sum > 1)
+                {
+                    GameObject copy = Instantiate(cube, cubePos, Quaternion.identity);
+                    copy.GetComponent<MengerBox>().size = (size / 3f);
+                    copy.transform.localScale = new Vector3((size / 3f)*3f , (size / 3f)*3f, (size / 3f)*3f);
+
+                    subCubes.Add(copy);
+                }
+            }
+        }
+    }
+     }
+           return subCubes;
+        
     }
 }
